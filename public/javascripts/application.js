82e95faa0f1ecbed74af7f46b6008cee5eb24026
Tate Johnson <tate@tatey.com>
2009-06-28 15:07:40 +1000

    Added jQuery. Users can "close" flash notices. Users can scroll to top. Styled show action for QuotesController

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  $('#back_to_top').click(function() {
+    $.scrollTo(0, 800);
+  });
+  
+  $('.close').click(function() {
+    $('.close').parent().fadeOut();
+  });
+});
